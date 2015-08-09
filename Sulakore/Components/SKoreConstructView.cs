27bfc2c10d56ce2977fcb1a272bd9591fead0151
ArachisH <TheOldNutMan@gmail.com>
2015-08-09 09:43:20 -0700

    Moved/renamed some things.

--- Sulakore/Components/SKoreConstructView.cs
+++ Sulakore/Components/SKoreConstructView.cs
@@ -22,6 +22,7 @@
     See License.txt in the project root for license information.
 */
 
+using System;
 using System.Windows.Forms;
 using System.ComponentModel;
 using System.Collections.Generic;
@@ -37,67 +38,61 @@ namespace Sulakore.Components
 
         private readonly HMessage _packet;
 
-        public int Length
-        {
-            get { return _packet.Length; }
-        }
         public ushort Header
         {
             get { return _packet.Header; }
             set { _packet.Header = value; }
         }
-        public IReadOnlyList<object> ChunksWritten
-        {
-            get { return _packet.ValuesWritten; }
-        }
+        public int Length => _packet.Length;
+        public IReadOnlyList<object> ValuesWritten => _packet.ValuesWritten;
 
         public SKoreConstructView()
         {
             _packet = new HMessage(0);
         }
 
-        public void Append(params object[] chunks)
+        public void ReplaceItem(object chunk)
+        {
+            ListViewItem item = SelectedItems[0];
+            _packet.ReplaceWritten(item.Index, chunk);
+            ListViewItem.ListViewSubItemCollection subItems = item.SubItems;
+
+            subItems[0].Text = chunk.GetType().Name
+                .Replace("Int32", "Integer");
+
+            byte[] data = HMessage.Encode(chunk);
+            subItems[1].Text = chunk.ToString();
+            subItems[2].Text = HMessage.ToString(data);
+
+            item.ToolTipText = string.Format(CHUNK_TIP,
+                subItems[0].Text, subItems[1].Text, data.Length, subItems[2].Text);
+        }
+        public void Write(params object[] chunks)
         {
             _packet.Write(chunks);
             try
             {
                 BeginUpdate();
                 ListViewItem item = null;
-                byte[] data = new byte[0];
                 SuppressItemSelectedEvent = true;
-                string typeName = string.Empty, value = string.Empty, encoded = string.Empty;
+
                 foreach (object chunk in chunks)
                 {
-                    value = chunk.ToString();
-
-                    data = HMessage.Encode(chunk);
-                    encoded = HMessage.ToString(data);
-                    typeName = chunk.GetType().Name.Replace("Int32", "Integer");
+                    string value = chunk.ToString();
+                    byte[] data = HMessage.Encode(chunk);
+                    string encoded = HMessage.ToString(data);
+                    string typeName = chunk.GetType().Name.Replace("Int32", "Integer");
 
                     item = FocusAdd(typeName, value, encoded);
                     item.ToolTipText = string.Format(CHUNK_TIP, typeName, value, data.Length, encoded);
                 }
+
                 SuppressItemSelectedEvent = false;
                 OnItemSelected(new ListViewItemSelectionChangedEventArgs(item, item.Index, true));
             }
             finally { EndUpdate(); }
         }
 
-        public void ReplaceItem(object chunk)
-        {
-            ListViewItem item = SelectedItems[0];
-            _packet.ReplaceWritten(item.Index, chunk);
-
-            item.SubItems[0].Text = chunk.GetType().Name
-                .Replace("Int32", "Integer");
-
-            byte[] data = HMessage.Encode(chunk);
-            item.SubItems[1].Text = chunk.ToString();
-            item.SubItems[2].Text = HMessage.ToString(data);
-
-            item.ToolTipText = string.Format(CHUNK_TIP,
-                item.SubItems[0].Text, item.SubItems[1].Text, data.Length, item.SubItems[2].Text);
-        }
         protected override void RemoveItem(ListViewItem listViewItem)
         {
             _packet.RemoveWritten(listViewItem.Index);
@@ -114,18 +109,13 @@ namespace Sulakore.Components
             base.MoveItemDown(listViewItem);
         }
 
-        public void ClearItems()
+        public void ClearWritten()
         {
             Items.Clear();
             _packet.ClearWritten();
+            OnItemsDeselected(EventArgs.Empty);
         }
-        public byte[] GetBytes()
-        {
-            return _packet.ToBytes();
-        }
-        public string GetString()
-        {
-            return _packet.ToString();
-        }
+        public byte[] GetBytes() => _packet.ToBytes();
+        public string GetString() => _packet.ToString();
     }
 }
