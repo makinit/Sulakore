aa1303eed77905052c5b5bfef9e4aec01f53b6ad
ArachisH <TheOldNutMan@gmail.com>
2015-09-02 12:22:31 -0700

    Added constructor to take in a char that represenets a map level.

--- Sulakore/Habbo/HPoint.cs
+++ Sulakore/Habbo/HPoint.cs
@@ -33,12 +33,10 @@ namespace Sulakore.Habbo
         /// Gets or sets the x-coordinate of the <see cref="HPoint"/>.
         /// </summary>
         public int X { get; set; }
-
         /// <summary>
         /// Gets or sets the y-coordinate of the <see cref="HPoint"/>.
         /// </summary>
         public int Y { get; set; }
-
         /// <summary>
         /// Gets or sets the z-coordinate of the <see cref="HPoint"/>.
         /// </summary>
@@ -64,12 +62,47 @@ namespace Sulakore.Habbo
             Y = y;
             Z = z;
         }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HPoint"/> class with the specified floor object coordinates.
+        /// </summary>
+        /// <param name="x">The horizontal position of the floor object.</param>
+        /// <param name="y">The vertical position of the floor object.</param>
+        /// <param name="level">The UTF-16 character that represent a floor level in the room map.</param>
+        public HPoint(int x, int y, char level)
+            : this(x, y, ToZ(level))
+        { }
+
+        public static double ToZ(char level)
+        {
+            if (level >= '0' && level <= '9')
+            {
+                return (level - 48);
+            }
+            else if (level >= 'a' && level <= 't')
+            {
+                return (level - 87);
+            }
+            return 0;
+        }
+        public static char ToLevel(double z)
+        {
+            char level = 'x';
+            if (z >= 0 && z <= 9)
+            {
+                level = (char)(z + 48);
+            }
+            else if (z >= 10 && z <= 29)
+            {
+                level = (char)(z + 87);
+            }
+            return level;
+        }
 
         /// <summary>
         /// Converts the <see cref="HPoint"/> to a human-readable string.
         /// </summary>
         /// <returns></returns>
         public override string ToString() =>
-            $"{{X={X},Y={Y},Z={Z}}}";
+            $"X: {X}, Y: {Y}, Z: {Z}";
     }
 }
