5368d871cb5f999357dfcb2cf72cbdfa7ab43580
ArachisH <theoldnutman@gmail.com>
2015-12-10 16:42:46 -0800

    Description change.

--- Sulakore/Properties/AssemblyInfo.cs
+++ Sulakore/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sulakore")]
-[assembly: AssemblyDescription("Unofficial Habbo Hotel SDK.")]
+[assembly: AssemblyDescription("Unofficial Habbo Hotel SDK")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ArachisH")]
 [assembly: AssemblyProduct("Sulakore")]
