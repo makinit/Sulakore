72030a3a987b3d06b6e86a655b625db8204a5e54
ArachisH <theoldnutman@gmail.com>
2016-02-19 19:41:51 -0800

    Added properties for getting/setting values of an HSchedule object. Some refactoring on how a ListViewItem is updated when an HSchedule object is changed/modified.

--- Sulakore/Components/SKoreScheduleView.cs
+++ Sulakore/Components/SKoreScheduleView.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Windows.Forms;
 using System.ComponentModel;
 using System.Collections.Generic;
@@ -8,10 +7,11 @@ using Sulakore.Protocol;
 
 namespace Sulakore.Components
 {
+    [DesignerCategory("Code")]
     public class SKoreScheduleView : SKoreListView
     {
+        private readonly Action<HSchedule> _updateItem;
         private readonly Dictionary<HSchedule, ListViewItem> _items;
-        private readonly Action<HSchedule, ScheduleTickEventArgs, bool> _updateItem;
 
         public event EventHandler<ScheduleTickEventArgs> ScheduleTick;
         protected virtual void OnScheduleTick(object sender, ScheduleTickEventArgs e)
@@ -24,8 +24,8 @@ namespace Sulakore.Components
                 ScheduleTick?.Invoke(this, e);
                 string newPacket = e.Packet.ToString();
 
-                if (e.Interval > 0 && schedule.Interval != e.Interval ||
-                    oldpacket != newPacket && !e.Packet.IsCorrupted)
+                if (oldpacket != newPacket ||
+                    e.Interval > 0 && schedule.Interval != e.Interval)
                 {
                     isDirty = true;
                 }
@@ -34,7 +34,79 @@ namespace Sulakore.Components
             finally
             {
                 if (isDirty || e.Cancel)
-                    UpdateItem(schedule, e, isDirty);
+                    UpdateItem(schedule);
+            }
+        }
+
+        protected HSchedule SelectedSchedule
+        {
+            get
+            {
+                if (!HasSelectedItem)
+                    return null;
+
+                var schedule =
+                    (SelectedItem.Tag as HSchedule);
+
+                return schedule;
+            }
+        }
+
+        public int SelectedCycles
+        {
+            get { return SelectedSchedule?.Cycles ?? -1; }
+            set
+            {
+                if (SelectedSchedule?.Cycles != value)
+                {
+                    SelectedSchedule.Cycles = value;
+                    UpdateItem(SelectedSchedule);
+                }
+            }
+        }
+        public int SelectedInterval
+        {
+            get { return SelectedSchedule?.Interval ?? -1; }
+            set
+            {
+                if (SelectedSchedule?.Interval != value)
+                {
+                    SelectedSchedule.Interval = value;
+                    UpdateItem(SelectedSchedule);
+                }
+            }
+        }
+        public string SelectedPacket
+        {
+            get { return SelectedSchedule?.Packet.ToString(); }
+            set
+            {
+                if (SelectedSchedule?.Packet.ToString() != value)
+                {
+                    var packet = new HMessage(value, SelectedDestination);
+                    if (packet.IsCorrupted) return;
+
+                    SelectedSchedule.Packet = packet;
+                    UpdateItem(SelectedSchedule);
+                }
+            }
+        }
+        public HDestination SelectedDestination
+        {
+            get
+            {
+                return (SelectedSchedule?
+                    .Packet.Destination ?? HDestination.Client);
+            }
+            set
+            {
+                if (SelectedSchedule?.Packet.Destination != value)
+                {
+                    SelectedSchedule
+                        .Packet.Destination = value;
+
+                    UpdateItem(SelectedSchedule);
+                }
             }
         }
 
@@ -60,19 +132,22 @@ namespace Sulakore.Components
             item.Checked = autoStart;
         }
 
-        protected override void RemoveItem(ListViewItem item, bool selectNext)
+        protected virtual void UpdateItem(HSchedule schedule)
         {
-            var schedule = (item.Tag as HSchedule);
-            if (schedule != null)
+            if (InvokeRequired) BeginInvoke(_updateItem, schedule);
+            else
             {
-                if (_items.ContainsKey(schedule))
-                    _items.Remove(schedule);
+                ListViewItem item = _items[schedule];
 
-                schedule.Dispose();
+                if (item.Checked != schedule.IsRunning)
+                    item.Checked = schedule.IsRunning;
+
+                item.SubItems[0].Text = schedule.Packet.ToString();
+                item.SubItems[1].Text = schedule.Packet.Destination.ToString();
+                item.SubItems[2].Text = schedule.Interval.ToString();
+                item.SubItems[3].Text = schedule.Cycles.ToString();
             }
-            base.RemoveItem(item, selectNext);
         }
-
         protected override void OnItemChecked(ItemCheckedEventArgs e)
         {
             var schedule = (e.Item.Tag as HSchedule);
@@ -86,26 +161,17 @@ namespace Sulakore.Components
             }
             base.OnItemChecked(e);
         }
-        protected virtual void UpdateItem(HSchedule schedule, ScheduleTickEventArgs e, bool isDirty)
+        protected override void RemoveItem(ListViewItem item, bool selectNext)
         {
-            if (InvokeRequired) BeginInvoke(_updateItem, schedule, e, isDirty);
-            else
+            var schedule = (item.Tag as HSchedule);
+            if (schedule != null)
             {
-                if (e.Cancel)
-                    _items[schedule].Checked = false;
+                if (_items.ContainsKey(schedule))
+                    _items.Remove(schedule);
 
-                if (isDirty)
-                {
-                    schedule.Packet = e.Packet;
-                    schedule.Interval = e.Interval;
-
-                    ListViewItem item = _items[schedule];
-                    item.SubItems[0].Text = e.Packet.ToString();
-                    item.SubItems[1].Text = e.Packet.Destination.ToString();
-                    item.SubItems[2].Text = e.Interval.ToString();
-                    item.SubItems[3].Text = e.Cycles.ToString();
-                }
+                schedule.Dispose();
             }
+            base.RemoveItem(item, selectNext);
         }
     }
 }
